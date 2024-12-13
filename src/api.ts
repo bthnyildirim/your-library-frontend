75c11c65830d20268665e341df5523d5943c2b6b
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-12-13 12:40:30 +0100

    fix URL in api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5005/api/books";
+  `${process.env.REACT_APP_API_URL}/books` || "http://localhost:5005/api/books";
 
 export const getBooks = async () => {
   try {
